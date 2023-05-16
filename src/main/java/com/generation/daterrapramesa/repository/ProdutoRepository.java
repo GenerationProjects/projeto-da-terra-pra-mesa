b4a1a5040f255b60e9a8403cc281712574068526
unknown <wesgaspar1@gmail.com>
2023-05-16 16:19:27 -0300

    Criação de repository, controller e semeação do banco teste

---
+++ src/main/java/com/generation/daterrapramesa/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.generation.daterrapramesa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.daterrapramesa.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+}
