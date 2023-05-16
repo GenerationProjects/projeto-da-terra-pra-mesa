65e67356d2e10d124e8ca8c781cc26c654f31366
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-16 14:37:45 -0300

    Inicialização do projeto integrador

---
+++ src/test/java/com/generation/daterrapramesa/DaTerraPraMesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.daterrapramesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaTerraPraMesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
