dd4e03cce5b0a8642008ee2c62bb105e858e7794
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-18 12:02:43 -0300

    Atualizando projeto

--- src/main/java/com/generation/daterrapramesa/controller/UsuarioController.java
+++ src/main/java/com/generation/daterrapramesa/controller/UsuarioController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/usuarios")
 @CrossOrigin(origins="*",allowedHeaders = "*")
 public class UsuarioController {
-
+  
 }
