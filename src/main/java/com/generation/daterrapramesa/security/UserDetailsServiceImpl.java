bb49623c77e0dc9e7f8f42acc01f1a9571f1f265
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-30 13:55:48 -0300

    Resolvendo problemas de Autenticação

--- src/main/java/com/generation/daterrapramesa/security/UserDetailsServiceImpl.java
+++ src/main/java/com/generation/daterrapramesa/security/UserDetailsServiceImpl.java
@@ -20,8 +20,8 @@ public class UserDetailsServiceImpl implements UserDetailsService{
 	private UsuarioRepository usuarioRepository;
 	
 	@Override
-	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException{
-		Optional<Usuario> usuario = usuarioRepository.findByUsuario(userName);
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+		Optional<Usuario> usuario = usuarioRepository.findByEmail(email);
 		
 		if(usuario.isPresent())
 			return new UserDetailsImpl(usuario.get());
