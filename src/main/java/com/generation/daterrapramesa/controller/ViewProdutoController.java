cfe22fd86e4881c1523d9fa409362053feee3899
Felipe Sousa <felipe.fps09@hotmail.com>
2023-06-23 11:54:52 -0300

    Configurando cors do ViewProdutoController.java

--- src/main/java/com/generation/daterrapramesa/controller/ViewProdutoController.java
+++ src/main/java/com/generation/daterrapramesa/controller/ViewProdutoController.java
@@ -5,15 +5,13 @@ import com.generation.daterrapramesa.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/view-produtos")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ViewProdutoController {
 
     @Autowired
